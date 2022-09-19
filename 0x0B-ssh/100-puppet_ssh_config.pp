4941ca52da38859d34bb36ca18fe2be5e399b26f
chacha-a-chacha <calex2607@gmail.com>
2022-09-19 14:56:23 +0000

    alt config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-# Creates a new 4096-byte RSA key pair named holberton.
-ssh-keygen -b 4096 -N betty -f holberton
+# Client configuration file with Puppet
+file_line {  'passw_no_autentication':
+	     path => '/etc/ssh/ssh_config',
+	     line => 'PasswordAuthentication no',
+	  }
+file_line {  'passw_direction':
+	     path => '/etc/ssh/ssh_config',
+	     line => 'IdentityFile ~/.ssh/school',
+	     }
