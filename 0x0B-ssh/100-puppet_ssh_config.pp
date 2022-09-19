af3396cdea1d6da7fbc1437f256ee859b73b0cc7
chacha-a-chacha <calex2607@gmail.com>
2022-09-19 15:00:05 +0000

    alt config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Your SSH client configuration must be configured to use the private key ~/.ssh/school
+# Your SSH client configuration must be configured to refuse to authenticate using a password
 # Client configuration file with Puppet
 file_line {  'passw_no_autentication':
 	     path => '/etc/ssh/ssh_config',
